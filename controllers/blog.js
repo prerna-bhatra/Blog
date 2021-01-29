cbe71dd04971f4379993e1faf62b142448f95253
unknown <bhatraprerna061@gmail.com>
2021-01-29 15:01:38 +0530

    read less than 5 index blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -211,7 +211,7 @@ exports.ReadBlogById=(req,res)=>
 													})
 						  			}
 						  			
-						  			else if(data.isUser===1 || data.ReadCount.indexOf(id)!=-1)
+						  			else if(data.isUser===1 || data.ReadCount.indexOf(id)>4)
 						  			{
 						  									Blog.findById(id).select("-BlogImg").exec( (err,data)=>
 													{
