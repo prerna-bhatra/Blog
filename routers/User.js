62d28afc484a55bffb9421d34642aa82fbd48a55
unknown <bhatraprerna061@gmail.com>
2021-01-29 17:14:31 +0530

    get

--- routers/User.js
+++ routers/User.js
@@ -5,7 +5,7 @@ const {signup,signin,signout}=require('../controllers/User');
 
 router.post("/signup",signup)
 router.post("/signin/:fingerprint",signin)
-router.post("/signout/:fingerprint",signout)
+router.get("/signout/:fingerprint",signout)
 
 
 module.exports=router;
