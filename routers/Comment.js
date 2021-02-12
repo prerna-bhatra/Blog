43aef0cfe4fce8cd5973017d144e2d35339ef8ea
unknown <bhatraprerna061@gmail.com>
2021-02-12 12:39:49 +0530

    mycomment length

--- routers/Comment.js
+++ routers/Comment.js
@@ -4,13 +4,13 @@ const router=express.Router();
 const {
 	userById
 }=require('../controllers/User');
-const {comment,ReadComment,MyComments}=require('../controllers/comments');
+const {comment,ReadComment,MyComments,MyCommentsLength}=require('../controllers/comments');
 const {BlogById}=require('../controllers/blog')
 
 router.post("/comment/:userId/:blogId",comment)
 router.get("/MyComments/:userId/:blogId",MyComments)
 router.get("/comments/:blogId",ReadComment)
-
+router.get('/MyCommentsLn/:userId/:blogId',MyCommentsLength)
 router.param('userId',userById)
 router.param("blogId",BlogById)
 module.exports=router;
